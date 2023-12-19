bf068f1963a0cacc324e334cd95a96834081a043
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-19 22:24:05 +0530

    Zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import Images from "./Images";
 import "./index.css";
-import "./assets/1.jpg";
 import { useState } from "react";
+
 const App = () => {
-  const images = [
-    "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V3_WG/Peridot/AZ2097-8x6_V3_WG_Peridot_Diamond_Diamond.jpg",
-    "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V4_WG/Peridot/AZ2097-8x6_V4_WG_Peridot_Diamond_Diamond.jpg",
-    "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V5_WG/Peridot/AZ2097-8x6_V5_WG_Peridot_Diamond_Diamond.jpg",
-    "https://cdn.azeera.com/product/design/AZ2097-8x6.jpg",
-    "https://www.azeera.com/public/AppraisalFiles/files/az2097-8x6-pr-dd-dd-wg14k.jpg?v=11",
-  ];
+  const images = Images;
   const [imageSelected, setImageSelected] = useState(images[0]);
+  // eslint-disable-next-line no-unused-vars
   const hoverHandle = (image, index) => {
     setImageSelected(image);
+  };
+
+  const [zoomLevel, setZoomLevel] = useState(100);
+
+  const handleZoomChange = (event) => {
+    setZoomLevel(event.target.value);
+  };
+
+  const handleZoomIn = () => {
+    if (zoomLevel < 200) {
+      setZoomLevel(zoomLevel + 10);
+    }
+  };
+
+  const handleZoomOut = () => {
+    if (zoomLevel > 10) {
+      setZoomLevel(zoomLevel - 10);
+    }
   };
   return (
     <div className="h-[100vh] w-full flex  items-center justify-center bg-fuchsia-400">
@@ -32,15 +47,33 @@ const App = () => {
               </div>
             ))}
           </div>
-          <div className="left2">
+          <div
+            className="left2"
+            style={{
+              transform: `scale(${zoomLevel / 100})`,
+              transformOrigin: "top left",
+            }}
+          >
             <img
-              className="w-[500px] h-[100%] object-contain"
+              className="w-[500px] h-[100%] object-fill"
               src={imageSelected}
               alt=""
             />
           </div>
         </div>
-        <div className="right"></div>
+        <div>
+          {" "}
+          <button onClick={handleZoomIn}>Zoom In</button>
+          <input
+            type="range"
+            min="10"
+            max="200"
+            step="10"
+            value={zoomLevel}
+            onChange={handleZoomChange}
+          />
+          <button onClick={handleZoomOut}>Zoom Out</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/Images.ts
@@ -0,0 +1,8 @@
+const Images = [
+  "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V3_WG/Peridot/AZ2097-8x6_V3_WG_Peridot_Diamond_Diamond.jpg",
+  "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V4_WG/Peridot/AZ2097-8x6_V4_WG_Peridot_Diamond_Diamond.jpg",
+  "https://cdn.azeera.com/product/AZ2097-8x6/AZ2097-8x6_V5_WG/Peridot/AZ2097-8x6_V5_WG_Peridot_Diamond_Diamond.jpg",
+  "https://cdn.azeera.com/product/design/AZ2097-8x6.jpg",
+  "https://www.azeera.com/public/AppraisalFiles/files/az2097-8x6-pr-dd-dd-wg14k.jpg?v=11",
+];
+export default Images;
